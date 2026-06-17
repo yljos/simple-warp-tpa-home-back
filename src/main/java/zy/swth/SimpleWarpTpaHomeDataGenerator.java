a32f4f311056d7fc20bc39fa703f668817adfdab
紫芸SaMa <116881803+ziy-ziy@users.noreply.github.com>
2026-06-17 21:25:34 +0800

    Add files via upload

---
+++ src/main/java/zy/swth/SimpleWarpTpaHomeDataGenerator.java
@@ -0,0 +1,12 @@
+package zy.swth;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jspecify.annotations.NonNull;
+
+public class SimpleWarpTpaHomeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(@NonNull FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
